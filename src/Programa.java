80a47368561dfe5c0af1688350f882d3729d3040
Lucas Fogaça <lucasfgc00@gmail.com>
2015-08-31 23:43:03 -0300

    Update
    Erro ao comparar String persiste.

--- src/Programa.java
+++ src/Programa.java
@@ -37,9 +37,50 @@ public class Programa {
 		
 		case 3:{
 			String nome;
+			int p=0;
 			System.out.println("Informe o nome do funcionario que quer deletar:");
 			nome=tc.next();
 			
+			for(int i=0; i<count; i++){
+				if(nome.equals(f[i].getNome())){
+					p=i;
+				}
+				
+			}
+			
+			if(p==count){
+				count--;
+			}
+			else{
+				for(int i=p; i<count; i++){
+					f[i]=f[i+1];
+				}
+			}
+			
+			break;
+		}
+		
+		case 4:{
+			int max=0;
+			int min =9999999;
+			int p=0;
+			int pp=0;
+			for(int i=0; i<count; i++){
+				if(f[i].getSalario()<min){
+					p=i;
+				}
+			}
+			System.out.println("O menor salario é do Funcionario: " + f[p].getNome());
+			System.out.println("");
+			
+			for(int i=0; i>max; i++){
+				if(f[i].getSalario()>max){
+					pp=i;
+				}
+			}
+			System.out.println("O menor salario é do Funcionario: " + f[pp].getNome());
+			System.out.println("");
+		break;
 		}
 		
 		}
